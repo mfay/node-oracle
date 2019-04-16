ddefeccc0b2f09994d31943cc52d1576b9cdd2bc
Mark Fay <faymark@gvsu.edu>
2019-04-16 16:43:53 -0400

    simple node express app talking to oracle

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const oracledb = require('oracledb');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/db', async (req, res, next) => {
+	let connection = await oracledb.getConnection({
+		user: 'system',
+		password: 'oracle1',
+		connectString: 'oracle/XE'
+	});
+
+	let results = await connection.execute(
+		'select * from hr.departments order by department_name',
+		[],
+		{
+			outFormat: oracledb.OBJECT
+		}
+	);
+
+	res.render('departments', { title: 'Departments', results: results.rows });
+});
+
+module.exports = router;
